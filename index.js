794badbb9225449bae043a9ca301b8be8a84b349
Emerson <emerson.day@gmail.com>
2022-10-30 13:53:13 -0700

    created mutations

--- index.js
+++ index.js
@@ -1,65 +1,65 @@
-var { graphqlHTTP } = require("express-graphql");
-var { buildSchema, assertInputType } = require("graphql");
-var express = require("express");
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema, assertInputType } = require('graphql');
+var express = require('express');
 
 // Construct a schema, using GraphQL schema language
 var restaurants = [
-  {
-    id: 1,
-    name: "WoodsHill ",
-    description:
-      "American cuisine, farm to table, with fresh produce every day",
-    dishes: [
-      {
-        name: "Swordfish grill",
-        price: 27,
-      },
-      {
-        name: "Roasted Broccily ",
-        price: 11,
-      },
-    ],
-  },
-  {
-    id: 2,
-    name: "Fiorellas",
-    description:
-      "Italian-American home cooked food with fresh pasta and sauces",
-    dishes: [
-      {
-        name: "Flatbread",
-        price: 14,
-      },
-      {
-        name: "Carbonara",
-        price: 18,
-      },
-      {
-        name: "Spaghetti",
-        price: 19,
-      },
-    ],
-  },
-  {
-    id: 3,
-    name: "Karma",
-    description:
-      "Malaysian-Chinese-Japanese fusion, with great bar and bartenders",
-    dishes: [
-      {
-        name: "Dragon Roll",
-        price: 12,
-      },
-      {
-        name: "Pancake roll ",
-        price: 11,
-      },
-      {
-        name: "Cod cakes",
-        price: 13,
-      },
-    ],
-  },
+    {
+        id: 1,
+        name: 'WoodsHill ',
+        description:
+            'American cuisine, farm to table, with fresh produce every day',
+        dishes: [
+            {
+                name: 'Swordfish grill',
+                price: 27,
+            },
+            {
+                name: 'Roasted Broccily ',
+                price: 11,
+            },
+        ],
+    },
+    {
+        id: 2,
+        name: 'Fiorellas',
+        description:
+            'Italian-American home cooked food with fresh pasta and sauces',
+        dishes: [
+            {
+                name: 'Flatbread',
+                price: 14,
+            },
+            {
+                name: 'Carbonara',
+                price: 18,
+            },
+            {
+                name: 'Spaghetti',
+                price: 19,
+            },
+        ],
+    },
+    {
+        id: 3,
+        name: 'Karma',
+        description:
+            'Malaysian-Chinese-Japanese fusion, with great bar and bartenders',
+        dishes: [
+            {
+                name: 'Dragon Roll',
+                price: 12,
+            },
+            {
+                name: 'Pancake roll ',
+                price: 11,
+            },
+            {
+                name: 'Cod cakes',
+                price: 13,
+            },
+        ],
+    },
 ];
 var schema = buildSchema(`
 type Query{
@@ -92,35 +92,44 @@ type Mutation{
 // The root provides a resolver function for each API endpoint
 
 var root = {
-  restaurant: (arg) => {
-    // Your code goes here
-    return restaurants[arg.id];
-  },
-  restaurants: () => {
-    // Your code goes here
-    return restaurants;
-  },
-  setrestaurant: ({ input }) => {
-    // Your code goes here
-    
-  },
-  deleterestaurant: ({ id }) => {
-    // Your code goes here
-  },
-  editrestaurant: ({ id, ...restaurant }) => {
-    // Your code goes here
-  },
+    restaurant: (arg) => {
+        return restaurants[arg.id];
+    },
+    restaurants: () => {
+        return restaurants;
+    },
+    setrestaurant: ({ input }) => {
+        restaurants.push({ name: input.name, description: input.description });
+        return input;
+    },
+    deleterestaurant: ({ id }) => {
+        const ok = Boolean(restaurants[id]);
+        let delc = restaurants[id];
+        restaurants = restaurants.filter((item) => item.id !== id);
+        console.log(JSON.stringify(delc));
+        return { ok };
+    },
+    editrestaurant: ({ id, ...restaurant }) => {
+        if (!restaurant[id]) {
+            throw new Error('contact does not exist');
+        }
+        restaurants[id] = {
+            ...restaurants[id],
+            ...restaurant,
+        };
+        return restaurants[id];
+    },
 };
 var app = express();
 app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-  })
+    '/graphql',
+    graphqlHTTP({
+        schema: schema,
+        rootValue: root,
+        graphiql: true,
+    })
 );
 var port = 5500;
-app.listen(5500, () => console.log("Running Graphql on Port:" + port));
+app.listen(5500, () => console.log('Running Graphql on Port:' + port));
 
 // export default root;
